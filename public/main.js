1ab30f466d4f35fcf93172363e33c559be3c9526
Daniel D Demerin <ddemerin@gmail.com>
2020-03-16 13:56:13 -0400

    finished update team buttons

--- public/main.js
+++ public/main.js
@@ -4,4 +4,25 @@ const main = () => {
   }
 }
 
+const updateTeamName = (teamInput, teamName) => {
+  const updateName = document.querySelector(teamInput).value
+  document.querySelector(teamName).textContent = updateName
+}
+
+const updateTeamOne = () => {
+  updateTeamName('.team-1-input', '.team-1-name')
+}
+
+const updateTeamTwo = () => {
+  updateTeamName('.team-2-input', '.team-2-name')
+}
+
 document.addEventListener('DOMContentLoaded', main)
+
+document
+  .querySelector('.update-team-1-name')
+  .addEventListener('click', updateTeamOne)
+document
+  .querySelector('.update-team-2-name')
+  .addEventListener('click', updateTeamTwo)
+document.querySelector('.')
